70fa4ec9ed6d77ba11b2bd3a5aa9f581ea044f6c
CavalheiroDev <ga3-cavalheiro@hotmail.com>
2021-08-22 12:39:19 -0300

    Criando as rotas /user da aplicação.

---
+++ app/api/routesUser.py
@@ -0,0 +1,34 @@
+from app.api.models import User
+from typing import List
+from fastapi import APIRouter, Depends, status, HTTPException
+from sqlalchemy.orm import Session
+from app.api.crud import get_all, post, put, delete
+from app.api.schemas import UserDB, UserQuery, UserSchema
+from app.db import SessionLocal
+
+
+router = APIRouter()
+
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+@router.get('/', response_model=List[UserDB], status_code=status.HTTP_200_OK)
+def read_all_users(db: Session = Depends(get_db)):
+
+    users = get_all(db=db)
+    if not users:
+        return HTTPException(404, 'Nenhum item encontrado.')
+    
+    return users
+
+
+@router.post('/', response_model=UserDB, status_code=status.HTTP_201_CREATED)
+def create_user(*, db: Session = Depends(get_db), data: UserSchema):
+    user = post(db=db, data=data)
+    return user

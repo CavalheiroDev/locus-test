32b17b22a0a97198238ed2dc23677309d65ceca2
CavalheiroDev <ga3-cavalheiro@hotmail.com>
2021-08-22 23:40:35 -0300

    Criando o a view principal da aplicação.

--- front/consumeApi/views.py
+++ front/consumeApi/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+
+def home(request):
+    response = requests.get('http://localhost:8080/users/page/1')
+    users = response.json()
+    return render(request, 'consumeApi/home.html', {'users': users})

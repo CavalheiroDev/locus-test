cc45fdbff78e0a6b8d329af92b3138461bde394f
CavalheiroDev <ga3-cavalheiro@hotmail.com>
2021-08-23 04:45:04 -0300

    Criando a view home.

--- front/consumeApi/views.py
+++ front/consumeApi/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 import requests
 
 
-def home(request):
-    response = requests.get('http://localhost:8080/users/page/1')
-    users = response.json()
-    return render(request, 'consumeApi/home.html', {'users': users})
+def home(request, number_page: int):
+    req = requests.get(f'http://localhost:8080/users/page/{number_page}')
+    response = req.json()
+    return render(request, 'index.html', {'users': response['items'], 'next_page': response['next_page'], 'previous_page': response['previous_page']})

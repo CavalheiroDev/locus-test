edf00ce1c444f3c4d4caa46d13a3b3fe94f3374e
CavalheiroDev <ga3-cavalheiro@hotmail.com>
2021-08-24 17:42:01 -0300

    Adicionando uma variavel para passar para o front end qual a ultima página com conteudo.

--- front/consumeApi/views.py
+++ front/consumeApi/views.py
@@ -5,4 +5,5 @@ import requests
 def home(request, number_page: int):
     req = requests.get(f'http://localhost:8080/users/page/{number_page}')
     response = req.json()
-    return render(request, 'index.html', {'users': response['items'], 'next_page': response['next_page'], 'previous_page': response['previous_page']})
+    last_page = response['total_pages'] - 1
+    return render(request, 'index.html', {'users': response['items'], 'next_page': response['next_page'], 'previous_page': response['previous_page'], 'total_pages': response['total_pages'], 'last_page': last_page})

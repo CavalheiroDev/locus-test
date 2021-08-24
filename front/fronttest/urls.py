a5a03eae7e20954bcc02a6122b4cf3687d71a7e4
CavalheiroDev <ga3-cavalheiro@hotmail.com>
2021-08-24 17:49:17 -0300

    Configurando a rota dos arquivos estaticos.

--- front/fronttest/urls.py
+++ front/fronttest/urls.py
@@ -13,13 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from fronttest.settings import STATIC_ROOT
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.views.generic import RedirectView
+from django.views.static import serve
 import consumeApi.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', RedirectView.as_view(url='/1')),
     path('<int:number_page>', consumeApi.views.home),
+    re_path(r'static/(?P<path>.*)$', serve,
+            {'document_root': STATIC_ROOT})
 ]
